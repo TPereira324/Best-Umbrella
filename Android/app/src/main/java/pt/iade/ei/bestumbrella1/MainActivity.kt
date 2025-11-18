3cb3f0b9d75705a2895e1679fb4ddc086e1b8664
TPereira324 <tahawurpereira1@gmail.com>
2025-11-18 21:50:17 +0000

    commit com tudo

--- Android/app/src/main/java/pt/iade/ei/bestumbrella1/MainActivity.kt
+++ Android/app/src/main/java/pt/iade/ei/bestumbrella1/MainActivity.kt
@@ -5,14 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
 import pt.iade.ei.bestumbrella1.MainNavigation.MainNavigation
